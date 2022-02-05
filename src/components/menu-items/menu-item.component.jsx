dd774f163b871cf01e809adb8c63efb20baabcce
Rohan Karmakar <rohankarmakar2356@gmail.com>
2022-02-05 08:51:56 +0530

    withRoter() MenuItems Added

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './menu-item.styles.scss'
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imgUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background' style={{
-            backgroundImage: `url(${imgUrl})`
+            backgroundImage: `url(${imageUrl})`
         }} >
         </div>
         <div className='content'>
@@ -14,4 +15,4 @@ const MenuItem = ({ title, imgUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
